4f739f284517191dea70d601b4259c9de5afee82
Shrishti <125468465+shrishti0308@users.noreply.github.com>
2024-12-22 18:32:23 +0530

    add data files for program details, footer, and header constants

---
+++ src/data/footerConstant.ts
@@ -0,0 +1,30 @@
+export const footerData = {
+    contactInfo: {
+      email: "hrd@theunivoc.com",
+    },
+    aboutus: [
+      { title: "About us", url: "/aboutus" },
+      { title: "Careers", url: "/careers" },
+    ],
+    academics: [{ title: "Programme", url: "/programme" }],
+    innovation: [
+      { title: "Student", url: "/student" },
+      { title: "Faculty", url: "/faculty" },
+    ],
+    otherLink: [
+      { title: "Jobs", url: "/jobs" },
+      { title: "Become A Teacher", url: "/teacher" },
+      { title: "Event", url: "/event" },
+    ],
+    socialLinks: [
+      { platform: "Facebook", url: "#" },
+      { platform: "Twitter", url: "#" },
+      { platform: "LinkedIn", url: "#" },
+      { platform: "Instagram", url: "#" },
+    ],
+    address:
+      "123 Random Building,\nRandom Street, Random City,\nRandom State - 123456",
+    slogan:
+      "Empowering individuals with the skills to thrive in a changing world. UNIVOC Skilling to Empower bridges talent and opportunity, driving innovation and growth for a brighter future.",
+  };
+  
---
+++ src/data/headerConstant.ts
@@ -0,0 +1,21 @@
+export const headerData = {
+    navbar: [
+      {
+        title: "Explore",
+        items: [
+          { title: "Courses", link: "/courses" },
+          { title: "Programs", link: "/programs" },
+          { title: "Categories", link: "/categories" },
+        ],
+      },
+      {
+        title: "Resources",
+        items: [
+          { title: "Blog", link: "/blog" },
+          { title: "Events", link: "/events" },
+          { title: "Webinars", link: "/webinars" },
+        ],
+      },
+    ],
+  };
+  
